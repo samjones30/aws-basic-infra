c96f999fcdcd3234074838ebed40f09d99d76cbc
Sam Jones <sam.jones34@nhs.net>
2020-01-31 14:56:22 +0000

    Initial SNS topic with SQS subcriber

--- main.tf
+++ main.tf
@@ -460,3 +460,13 @@ resource "aws_sqs_queue" "status_queue" {
     Terraform   = "true"
   }
 }
+
+resource "aws_sns_topic" "status_updates" {
+  name = "status-updates-topic"
+}
+
+resource "aws_sns_topic_subscription" "status_updates_sqs_target" {
+  topic_arn = "${aws_sns_topic.status_updates.arn}"
+  protocol  = "sqs"
+  endpoint  = "${aws_sqs_queue.status_queue.arn}"
+}
