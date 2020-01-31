8137941a0d85e2dcfe4818e71bfa5dc7f4a3e27c
Sam Jones <sam.jones34@nhs.net>
2020-01-31 14:38:05 +0000

    Updates to sonar DB param groups

--- main.tf
+++ main.tf
@@ -444,6 +444,23 @@ resource "aws_security_group" "web-lb-sg" {
 
   tags = {
     Name      = "web-lb-sg"
-    Terraform = true
+    Terraform = "true"
+  }
+}
+
+resource "aws_sqs_queue" "status_queue" {
+  name                      = "status-queue-example"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  redrive_policy            = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.status_queue_deadletter.arn
+    maxReceiveCount     = 4
+  })
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
   }
 }
